098a93b16767fb8eb2689d611b83ec7a59d7709e
Monil Andharia <monilandharia@Monils-MacBook-Pro.local>
2020-10-17 15:19:32 +0530

    Added logging and exception handling

--- bot.py
+++ bot.py
@@ -3,22 +3,28 @@ import requests
 import os
 import cv2
 import numpy as np
+import logging
 
 from praw.models import Comment
 from imgurpython import ImgurClient
 
 DIR = os.path.join(os.getcwd(), "posts")
+LOG_FILENAME = "bot.log"
 
 
 class Utils:
     def download_image(self, url):
-        if url.endswith((".jpg", ".jpeg", ".png", ".gif")):
-            img = requests.get(url).content
-            filename = os.path.basename(url)
-            fq_filename = os.path.join(DIR, filename)
-            with open(fq_filename, "wb") as f:
-                f.write(img)
-            return fq_filename
+        fq_filename = None
+        if url.endswith((".jpg", ".jpeg", ".png")):
+            try:
+                img = requests.get(url).content
+                filename = os.path.basename(url)
+                fq_filename = os.path.join(DIR, filename)
+                with open(fq_filename, "wb") as f:
+                    f.write(img)
+            except Exception as exception:
+                logging.exception(f"Could not download image :: {exception}")
+        return fq_filename
 
     def flip_image_halves(self, fq_filename):
         image = cv2.imread(fq_filename)
@@ -26,13 +32,14 @@ class Utils:
         cv2.imwrite(fq_filename, flipped_img)
 
     def upload_to_imgur(self, fq_filename):
-        env_variables = os.environ.items()
+        env_variables = dict(os.environ)
         try:
             client = ImgurClient(
-                env_variables["IMGUR_CLIENT_ID"],
-                env_variables["IMGUR_CLIENT_SECRET"],
+                env_variables.get("IMGUR_CLIENT_ID"),
+                env_variables.get("IMGUR_CLIENT_SECRET"),
             )
-        except Exception:
+        except Exception as exception:
+            logging.exception(f"Could not upload to imgur :: {exception}")
             return None
         return client.upload_from_path(fq_filename).get("link")
 
@@ -56,6 +63,7 @@ class ConvertToParallel:
         for comment in self.get_pending_replies():
             filename = utils.download_image(comment.submission.url)
             if not filename:
+                logging.error("No file to be processed")
                 continue
             utils.flip_image_halves(filename)
             hosted_link = utils.upload_to_imgur(filename)
@@ -63,6 +71,9 @@ class ConvertToParallel:
                 continue
             comment.reply(
                 f"Hey, I am a ParallelView bot developed by u/monilandharia. Your image has been converted and hosted at [imgur]({hosted_link}). You can trigger me by mentioning my name in a post's comment. Help me advance by contributing at [github](https://github.com/monil20/reddit-crossview-to-parallelview)."
+            )
+            logging.info(
+                f"Converted post {comment.submission.url} and hosted to {hosted_link}"
             )
             processed_images.append(comment)
             os.remove(filename)
@@ -74,5 +85,15 @@ class ConvertToParallel:
 if __name__ == "__main__":
     if not os.path.exists(DIR):
         os.makedirs(DIR)
-    bot = ConvertToParallel("parallelview-bot")
+    logging.basicConfig(
+        filename=LOG_FILENAME,
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+    try:
+        bot = ConvertToParallel("parallelview-bot")
+    except:
+        logging.error("Could not connect to Reddit")
+        exit(1)
     bot.convert_and_reply()
